d2f900df7a508cca18504cddbf663d7204d18e06
JC Coles <jcoles1155@gmail.com>
2021-04-13 22:41:24 -0700

    lab day1 finished

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    # route for cats index
+    path('pedals/', views.pedals_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,26 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def pedals_index(request):
+    return render(request, 'pedals/index.html', {'pedals': pedals})
+
+
+class Pedal:  # Note that parens are optional if not inheriting from another class
+    def __init__(self, name, type, description, price):
+        self.name = name
+        self.type = type
+        self.description = description
+        self.price = price
+
+
+pedals = [
+    Pedal('Fuzzface', 'Overdrive', 'Jimis fav', 300),
+    Pedal('HOF', 'Reverb', 'lots of reverb sounds', 200),
+    Pedal('Boss Compressor', 'Compressor', '10 Band compression', 40)
+]
